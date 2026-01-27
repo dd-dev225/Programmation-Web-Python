b3b89399313b0369ea46e3aca9235e353df848df
Dedson <dedson.dream@gmail.com>
2026-01-27 16:14:13 +0000

    feat(auth): Rename login.html -> login2.html and add register2.html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 app_name = 'dashboard'   # ← IMPORTANT Car permettant de spécifier le namespace de l'application pour définir les urls dans les pages web
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path("<str:segment>/liste/", views.segmentliste, name="segmentliste"),
     
     # Authentification
-    path('login/', auth_views.LoginView.as_view(template_name='dashboard/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='dashboard/login2.html'), name='login'),
+    path('register/', TemplateView.as_view(template_name='dashboard/register2.html'), name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='dashboard:login'), name='logout'),
 ]
